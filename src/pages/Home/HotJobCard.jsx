c6f257872a8918f06ff6dc89bffec027970b549e
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-12-15 13:58:54 +0600

    job cards created for home page

--- src/pages/Home/HotJobCard.jsx
+++ src/pages/Home/HotJobCard.jsx
@@ -1,10 +1,55 @@
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaDollarSign } from "react-icons/fa6";
 
-const HotJobCard = () => {
-    return (
+/* eslint-disable react/prop-types */
+const HotJobCard = ({ job }) => {
+  const {
+    _id,
+    title,
+    company,
+    company_logo,
+    requirements,
+    description,
+    location,
+    salaryRange,
+  } = job;
+  return (
+    <div className="card card-compact bg-base-100 shadow-xl">
+      <div className="flex gap-4 m-4">
+        <figure>
+          <img className="w-16" src={company_logo} alt="Shoes" />
+        </figure>
         <div>
-            
+          <h4 className="text-2xl">{company}</h4>
+          <p className="flex gap-1 items-center">
+            <FaMapMarkerAlt></FaMapMarkerAlt> {location}
+          </p>
         </div>
-    );
+      </div>
+      <div className="card-body">
+        <h2 className="card-title">
+          {title}
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>{description}</p>
+        <div className="flex gap-2 flex-wrap">
+          {requirements.map((skill, index) => (
+            <p
+              key={index}
+              className="border rounded-xl text-center px-2 py-1 hover:text-purple-700 
+              hover:bg-gray-200 cursor-pointer"
+            >
+              {skill}
+            </p>
+          ))}
+        </div>
+        <div className="card-actions justify-end items-center mt-4">
+            <p className="flex items-center">Salary:<FaDollarSign></FaDollarSign> {salaryRange.min} - {salaryRange.max} {salaryRange.currency}</p>
+          <button className="btn btn-sm btn-outline btn-primary">Apply</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HotJobCard;
--- src/pages/Home/HotJobs.jsx
+++ src/pages/Home/HotJobs.jsx
@@ -2,24 +2,24 @@ import { useEffect, useState } from "react";
 import HotJobCard from "./HotJobCard";
 
 const HotJobs = () => {
-    const [jobs, setJobs] = useState([]);
+  const [jobs, setJobs] = useState([]);
 
-    useEffect(()=>{
-        fetch(`http://localhost:5000/jobs`)
-        .then(res => res.json())
-        .then(data => {
-            setJobs(data);
-        })
-    },[])
-    return (
-        <div>
-            <div>
-                {
-                    jobs.map(job => <HotJobCard key={job._id} job={job}></HotJobCard>)
-                }
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    fetch(`http://localhost:5000/jobs`)
+      .then((res) => res.json())
+      .then((data) => {
+        setJobs(data);
+      });
+  }, []);
+  return (
+    <div className="my-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {jobs.map((job) => (
+          <HotJobCard key={job._id} job={job}></HotJobCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HotJobs;
